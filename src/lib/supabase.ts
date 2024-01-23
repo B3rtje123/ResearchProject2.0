abd687beb69a467285e49083265f81c48bf8aa0c
B3rtje123 <114138835+B3rtje123@users.noreply.github.com>
2024-01-23 02:38:30 +0100

    cart fixed and working needs to calc final price

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,19 +1,12 @@
-import { createServerClient } from '@supabase/ssr'
+import { createClient } from "@supabase/supabase-js";
 
 
-const { URL, SECRET_KEY } = import.meta.env
-
-if (!URL) {
-  throw new Error('Missing env: supabase_url')
-}
-if (!SECRET_KEY) {
-  throw new Error('Missing env: supabase_service_role')
-}
-
-export const supabase = createServerClient(
-  URL!,
-  SECRET_KEY!,
+export const supabase = createClient(
+  import.meta.env.URL,
+  import.meta.env.SECRET_KEY,
   {
-    cookies: {},
+    auth: {
+      flowType: "pkce",
+    },
   },
-)
+);
