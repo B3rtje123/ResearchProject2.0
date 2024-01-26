3b50d70c499b86ff5d0d29a65315384c81b65d49
B3rtje123 <114138835+B3rtje123@users.noreply.github.com>
2024-01-26 01:39:40 +0100

    DetailPage nearly done and cart done

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
